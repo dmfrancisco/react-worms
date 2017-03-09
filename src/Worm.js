2293e9f56ba2b80614e66f4c45783c2a833bf3e4
David Francisco <dmfrancisc@gmail.com>
2017-03-09 14:44:53 +0000

    Simplify use by other projects

--- src/Worm.js
+++ src/Worm.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
-import "./Worm.css";
 
 class Worm extends Component {
   getColor(score) {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Worm from "./Worm";
+import "./Worm.css";
 
 const Demo = () => (
   <div>
