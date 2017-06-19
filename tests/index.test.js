09985166e7170e066c6875b0dd5841a33314e141
David Francisco <dmfrancisc@gmail.com>
2017-06-19 22:23:21 +0100

    Add some simple snapshot tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,75 @@
-it("example", () => {
-  expect(1 + 2).toEqual(3);
+import React from "react";
+import { shallow } from "enzyme";
+
+import Worm from "../src/";
+
+it("renders correctly with only required props", () => {
+  const element = shallow(
+    <Worm
+      scores={[4, 8, 15, 16, 23, 42]}
+      min={0}
+      max={50}
+    />
+  );
+  expect(element.getNodes()).toMatchSnapshot();
+});
+
+it("supports custom labels", () => {
+  const element = shallow(
+    <Worm
+      scores={[4, 8, 15, 16, 23, 42]}
+      min={0}
+      max={50}
+      labels={["Lorem", "Ipsum", "Dolor", "Sit", "Amet", "Consectetur"]}
+    />
+  );
+  expect(element.getNodes()).toMatchSnapshot();
+});
+
+it("supports custom colors", () => {
+  const element = shallow(
+    <Worm
+      scores={[4, 8, 15, 16, 23, 42]}
+      min={0}
+      max={50}
+      bgdColor="#253b6b"
+      colors={["#c6e48b", "#7bc96f", "#196127"]}
+    />
+  );
+  expect(element.getNodes()).toMatchSnapshot();
+});
+
+it("supports custom styles", () => {
+  const element = shallow(
+    <Worm
+      scores={[4, 8, 15, 16, 23, 42]}
+      min={0}
+      max={50}
+      styles={{ color: "white", fontSize: "1.4em" }}
+    />
+  );
+  expect(element.getNodes()).toMatchSnapshot();
+});
+
+it("supports custom classNames", () => {
+  const element = shallow(
+    <Worm
+      scores={[4, 8, 15, 16, 23, 42]}
+      min={0}
+      max={50}
+      className="Worm--custom"
+    />
+  );
+  expect(element.getNodes()).toMatchSnapshot();
+});
+
+it("supports negative scores", () => {
+  const element = shallow(
+    <Worm
+      scores={[-5, -2, 0, 2, 5]}
+      min={-5}
+      max={5}
+    />
+  );
+  expect(element.getNodes()).toMatchSnapshot();
 });
